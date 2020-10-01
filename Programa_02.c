c58179fd6b746182c9cba56118ed2b788266eeb6
ppalmeros <71098644+ppalmeros@users.noreply.github.com>
2020-10-01 08:29:47 -0500

    Agregando programas

---
+++ Programa_02.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+	int a = 1, b = 2, c = 5, d = 7;
+
+	printf("a > b || c > d:  %d \n",  a > b || c > d  );
+	printf("a > b || c < d:  %d \n",  a > b || c < d  );
+	printf("a < b || c > d:  %d \n",  a < b || c > d  );
+	printf("a < b || c < d:  %d \n",  a < b || c < d  );
+
+}
+
+
+
+
---
+++ Programa_03.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+	int a = 1, b = 4;
+	
+	printf("a > b:    %d \n",   a > b   );
+	printf("!(a > b): %d \n", !(a > b)  );
+	
+	printf("a < b:    %d \n",   a < b  );
+	printf("!(a < b): %d \n", !(a < b) );
+
+}
+
+
+
+
